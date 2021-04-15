dfb869c9ab3ec168f92d971b0f0c05af5553a60a
SaouZADEV <82593060+SaouZADEV@users.noreply.github.com>
2021-04-16 00:23:30 +0700

    Add files via upload

--- Electricity_Calculator/src/com/electic_calculator/gui/main/Electric_Power2.java
+++ Electricity_Calculator/src/com/electic_calculator/gui/main/Electric_Power2.java
@@ -1,28 +1,26 @@
 package com.electic_calculator.gui.main;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Text;
-
-
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.wb.swt.SWTResourceManager;
 
-public class Electric_Power1 {
+public class Electric_Power2 {
 
 	protected Shell shell;
 	private Text inputP;
 	private Text inputV;
-	private Text inputI;
+	private Text inputR;
 	private Text outputP;
 	private Text outputV;
-	private Text outputI;
+	private Text outputR;
 
 	/**
 	 * Launch the application.
@@ -31,7 +29,7 @@ public class Electric_Power1 {
 	 */
 	public static void CalculatorFront() {
 		try {
-			Electric_Power1 window = new Electric_Power1();
+			Electric_Power2 window = new Electric_Power2();
 			window.open();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -69,9 +67,9 @@ public class Electric_Power1 {
 		lblV.setBounds(10, 231, 15, 20);
 		lblV.setText("V");
 		
-		Label lblI = new Label(shell, SWT.NONE);
-		lblI.setBounds(10, 283, 15, 20);
-		lblI.setText("I");
+		Label lblR = new Label(shell, SWT.NONE);
+		lblR.setBounds(10, 283, 15, 20);
+		lblR.setText("I");
 		
 		inputP = new Text(shell, SWT.BORDER);
 		inputP.setBounds(33, 173, 208, 26);
@@ -79,8 +77,8 @@ public class Electric_Power1 {
 		inputV = new Text(shell, SWT.BORDER);
 		inputV.setBounds(33, 228, 208, 26);
 		
-		inputI = new Text(shell, SWT.BORDER);
-		inputI.setBounds(33, 280, 208, 26);
+		inputR = new Text(shell, SWT.BORDER);
+		inputR.setBounds(33, 280, 208, 26);
 		
 		outputP = new Text(shell, SWT.BORDER);
 		outputP.setBounds(391, 173, 208, 26);
@@ -88,40 +86,37 @@ public class Electric_Power1 {
 		outputV = new Text(shell, SWT.BORDER);
 		outputV.setBounds(391, 228, 208, 26);
 		
-		outputI = new Text(shell, SWT.BORDER);
-		outputI.setBounds(391, 280, 208, 26);
+		outputR = new Text(shell, SWT.BORDER);
+		outputR.setBounds(391, 280, 208, 26);
 		
 		Button btnCalculate = new Button(shell, SWT.NONE);
 		btnCalculate.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
-				double p,v,i;
+				double p,v,r;
 				if (inputP.getText().equals("")){
 						v = Double.parseDouble(inputV.getText());
-						i = Double.parseDouble(inputI.getText());
-						//Electric_Power1_medthod pr1 = new Electric_Power1_medthod();
-						p = v*i;
+						r = Double.parseDouble(inputR.getText());
+						p = (v*v)/r;
 						outputP.setText(Double.toString(p));
 						outputV.setText(Double.toString(v));
-						outputI.setText(Double.toString(i));
+						outputR.setText(Double.toString(r));
 				}
 				else if(inputV.getText().equals("")){
 						p = Double.parseDouble(inputP.getText());
-						i = Double.parseDouble(inputI.getText());
-						//Electric_Power1_medthod pr1 = new Electric_Power1_medthod();
-						v= p/i;
+						r = Double.parseDouble(inputR.getText());
+						v= Math.sqrt(p*r);
 						outputP.setText(Double.toString(p));
 						outputV.setText(Double.toString(v));
-						outputI.setText(Double.toString(i));
+						outputR.setText(Double.toString(r));
 				}
-				else if(inputI.getText().equals("")) {
+				else if(inputR.getText().equals("")) {
 						p = Double.parseDouble(inputP.getText());
 						v = Double.parseDouble(inputV.getText());
-						//Electric_Power1_medthod pr1 = new Electric_Power1_medthod();
-						i = p/v;
+						r = (v*v)/v;
 						outputP.setText(Double.toString(p));
 						outputV.setText(Double.toString(v));
-						outputI.setText(Double.toString(i));
+						outputR.setText(Double.toString(r));
 			}
 			}
 		});
@@ -139,14 +134,12 @@ public class Electric_Power1 {
 		
 		Label lblNewLabel_1 = new Label(shell, SWT.NONE);
 		lblNewLabel_1.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
-		lblNewLabel_1.setBounds(284, 61, 69, 26);
-		lblNewLabel_1.setText("P = V*I");
+		lblNewLabel_1.setBounds(261, 61, 115, 26);
+		lblNewLabel_1.setText("P = V^2 / R");
 		
 		Label lblNewLabel_2 = new Label(shell, SWT.NONE);
 		lblNewLabel_2.setBounds(132, 98, 370, 56);
 		lblNewLabel_2.setText("Input the two variables that you know and leave the one \r\n\t          you are looking for empty");
 		
-		
 
-	}
-}
+}}
--- Electricity_Calculator/src/com/electic_calculator/gui/main/calculatorFront.java
+++ Electricity_Calculator/src/com/electic_calculator/gui/main/calculatorFront.java
@@ -52,11 +52,6 @@ public class calculatorFront {
 		shlElectricityEquationsCalculator.setText("Electricity Equations Calculator");
 		
 		Button Electric_pr1_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
-		Electric_pr1_formular.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			}
-		});
 		Electric_pr1_formular.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
@@ -64,7 +59,7 @@ public class calculatorFront {
 				pr1.CalculatorFront();
 			}
 		});
-		Electric_pr1_formular.setBounds(50, 210, 90, 30);
+		Electric_pr1_formular.setBounds(60, 210, 90, 30);
 		Electric_pr1_formular.setText("P = V * I");
 		
 		Label lblNewLabel = new Label(shlElectricityEquationsCalculator, SWT.NONE);
@@ -79,24 +74,87 @@ public class calculatorFront {
 		lblClickOnThe.setText("Click on the equation you want to calculator");
 		
 		Label lblElectricityFormulas = new Label(shlElectricityEquationsCalculator, SWT.NONE);
-		lblElectricityFormulas.setBounds(51, 169, 135, 20);
-		lblElectricityFormulas.setText("Electricity Formulas");
+		lblElectricityFormulas.setBounds(20, 169, 171, 20);
+		lblElectricityFormulas.setText("Electricity Power Formula");
 		
 		Label lblNewLabel_1 = new Label(shlElectricityEquationsCalculator, SWT.NONE);
-		lblNewLabel_1.setBounds(265, 190, 135, 20);
+		lblNewLabel_1.setBounds(265, 250, 135, 20);
 		lblNewLabel_1.setText("P = Power (watts, W)");
 		
 		Label lblNewLabel_2 = new Label(shlElectricityEquationsCalculator, SWT.NONE);
-		lblNewLabel_2.setBounds(265, 220, 151, 20);
+		lblNewLabel_2.setBounds(265, 280, 151, 20);
 		lblNewLabel_2.setText("V = Voltage (volts, V)");
 		
 		Label lblNewLabel_3 = new Label(shlElectricityEquationsCalculator, SWT.NONE);
 		lblNewLabel_3.setText("I = Current (ampare, A)");
-		lblNewLabel_3.setBounds(265, 250, 151, 20);
+		lblNewLabel_3.setBounds(265, 310, 151, 20);
 		
 		Label lblNewLabel_4 = new Label(shlElectricityEquationsCalculator, SWT.NONE);
-		lblNewLabel_4.setBounds(265, 280, 166, 20);
+		lblNewLabel_4.setBounds(265, 340, 166, 20);
 		lblNewLabel_4.setText("R = Resistance (ohm, \u03A9)");
+		
+		Button Electric_pr2_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_pr2_formular.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				Electric_Power2 pr2 = new Electric_Power2();
+				pr2.CalculatorFront();
+			}
+		});
+		Electric_pr2_formular.setBounds(60, 255, 90, 30);
+		Electric_pr2_formular.setText("P = V^2 / R");
+		
+		Button Electric_pr3_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_pr3_formular.setBounds(60, 304, 90, 30);
+		Electric_pr3_formular.setText("P = R * I^2");
+		
+		Label lblElectricityResistanceFormula = new Label(shlElectricityEquationsCalculator, SWT.NONE);
+		lblElectricityResistanceFormula.setBounds(20, 395, 195, 20);
+		lblElectricityResistanceFormula.setText("Electricity Resistance Formula");
+		
+		Button Electric_rs1_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_rs1_formular.setText("R = V / I");
+		Electric_rs1_formular.setBounds(60, 430, 90, 30);
+		
+		Button Electric_rs2_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_rs2_formular.setBounds(60, 476, 90, 30);
+		Electric_rs2_formular.setText("R = P / I^2");
+		
+		Button Electric_rs3_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_rs3_formular.setBounds(60, 522, 90, 30);
+		Electric_rs3_formular.setText("R = V^2 / P");
+		
+		Label lblElectricityVoltageFormula = new Label(shlElectricityEquationsCalculator, SWT.NONE);
+		lblElectricityVoltageFormula.setBounds(529, 169, 184, 20);
+		lblElectricityVoltageFormula.setText("Electricity Voltage Formula");
+		
+		Button Electric_vo1_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_vo1_formular.setBounds(567, 210, 90, 30);
+		Electric_vo1_formular.setText("New Button");
+		
+		Button Electric_vo2_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_vo2_formular.setBounds(567, 255, 90, 30);
+		Electric_vo2_formular.setText("New Button");
+		
+		Button Electric_vo3_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_vo3_formular.setBounds(567, 305, 90, 30);
+		Electric_vo3_formular.setText("New Button");
+		
+		Label lblElectricityCurrentFormula = new Label(shlElectricityEquationsCalculator, SWT.NONE);
+		lblElectricityCurrentFormula.setBounds(529, 395, 184, 20);
+		lblElectricityCurrentFormula.setText("Electricity Current Formula");
+		
+		Button Electric_cur1_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_cur1_formular.setBounds(567, 430, 90, 30);
+		Electric_cur1_formular.setText("New Button");
+		
+		Button Electric_cur2_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_cur2_formular.setBounds(567, 476, 90, 30);
+		Electric_cur2_formular.setText("New Button");
+		
+		Button Electric_cur3_formular = new Button(shlElectricityEquationsCalculator, SWT.NONE);
+		Electric_cur3_formular.setBounds(567, 522, 90, 30);
+		Electric_cur3_formular.setText("New Button");
 
 	}
 }
